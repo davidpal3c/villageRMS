e9057577b033ed8eca67626744b8d4aa135bac86
David Palacios <davidpal3c@gmail.com>
2024-08-02 12:49:30 -0600

    'added update and delete functionality'

--- Models/User.cs
+++ Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,24 +15,33 @@ namespace VillageRMS.Models
         private string _emailAddress;
         private string _status;
 
+        [Required(ErrorMessage = "Last name is required")]
+        [StringLength(50, ErrorMessage = "Last name cannot exceed 50 characters")]
         public string LastName
         {
             get { return _lastName; }
             set { _lastName = value; }
         }
 
+        [Required(ErrorMessage = "First name is required")]
+        [StringLength(50, ErrorMessage = "First name cannot exceed 50 characters")]
         public string FirstName
         {
             get { return _firstName; }
             set { _firstName = value; }
         }
 
+        [Required(ErrorMessage = "Phone number is required")]
+        [RegularExpression(@"^\d+$", ErrorMessage = "Phone number must be numeric")]
+        [StringLength(15, ErrorMessage = "Phone number cannot exceed 15 digits")]
         public string PhoneNumber
         {
             get { return _phoneNumber; }
             set { _phoneNumber = value; }
         }
 
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
         public string EmailAddress
         {
             get { return _emailAddress; }
--- Services/CustomerMapper.cs
+++ Services/CustomerMapper.cs
@@ -7,17 +7,28 @@ namespace VillageRMS.Services
     {
         public Customer MapFromReader(MySqlDataReader reader)
         {
-            Customer customer = new Customer();
-
-            return new Customer
+            try
             {
-                CustomerId = reader.GetInt32("CustomerID"),
-                LastName = reader.GetString("LastName"),
-                FirstName = reader.GetString("FirstName"),
-                PhoneNumber = reader.GetString("ContactPhone"),
-                EmailAddress = reader.GetString("Email"),
-                Status = reader.GetString("Status"),
-            };
+                Customer customer = new Customer();
+
+                //string _ph = reader.GetString("ContactPhone");
+                //int phoneNumber = Int32.Parse(_ph);
+
+                return new Customer
+                {
+                    CustomerId = reader.GetInt32("CustomerID"),
+                    LastName = reader.GetString("LastName"),
+                    FirstName = reader.GetString("FirstName"),
+                    PhoneNumber = reader.GetString("ContactPhone"),
+                    EmailAddress = reader.GetString("Email"),
+                    Status = reader.GetString("Status"),
+                };
+            }
+            catch (Exception ex) 
+            {
+                throw new Exception(ex.Message);
+            }
+            
         }
     }
 
