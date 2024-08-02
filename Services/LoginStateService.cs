11bf52ff3ca98581dec6ce3b58474844c7849dfe
glconde <georgelouie.conde@edu.sait.ca>
2024-08-01 22:46:27 -0600

    login modal and service

---
+++ Services/LoginStateService.cs
@@ -0,0 +1,25 @@
+﻿
+namespace VillageRMS.Services
+{
+    public class LoginStateService
+    {
+        public event Action OnChange;
+
+        private bool isLoggedIn;
+
+        public bool IsLoggedIn
+        {
+            get => isLoggedIn;
+            set
+            {
+                if (isLoggedIn != value)
+                {
+                    isLoggedIn = value;
+                    NotifyStateChanged();
+                }
+            }
+        }
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+    }
+}
